f8061601b3a85b50b50165c9fb49bddbd159adf5
root <root@bolt-qzjbuzngmn-m27a5yti87.bolt-pods.turi-bolt.svc.cluster.local>
2025-03-28 12:20:39 +0000

    flexible

--- scripts/run_btwn_domain_rm_ppi.sh
+++ scripts/run_btwn_domain_rm_ppi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if required arguments are provided
-if [ "$#" -lt 7 ]; then
-    echo "Usage: $0 <dataset_idx> <pretrain_model> <ppi_type> <batch_size> <learning_rate> <max_epoch> <percent_gold_label> <lambda> <pseudo_label_model>"
+if [ "$#" -lt 9 ]; then
+    echo "Usage: $0 <dataset_idx> <pretrain_model> <ppi_type> <batch_size> <learning_rate> <max_epoch> <percent_gold_label> <lambda> <pseudo_label_model> [wandb_project] [wandb_run_name]"
     exit 1
 fi
 
@@ -16,6 +16,8 @@ max_epoch=$6
 percent_gold_label=$7
 lbda=$8
 pseudo_label_model=$9
+custom_wandb_project=${10}
+custom_wandb_run_name=${11}
 
 # Dataset configurations
 target_datasets=("esfrankel17/HelpSteer2_binarized_w_weak_preferences_cleaned" "esfrankel17/ChatbotArena55k_binarized_w_weak_preferences_cleaned" "esfrankel17/ChatbotArena55k_binarized_w_weak_preferences_cleaned" "esfrankel17/Nectar_10_pct_subsample_binarized_w_weak_preferences_cleaned")
@@ -47,8 +49,18 @@ formatted_dataset_name=$(format_dataset_name ${dataset})
 formatted_target_dataset_name=$(format_dataset_name ${target_dataset})
 formatted_pretrain_model_name=$(format_pretrain_model_name ${pretrain_model})
 
-wandb_project=ppi-rm-btwn-domain-${formatted_dataset_name}-to-${formatted_target_dataset_name}-pretrain-${formatted_pretrain_model_name}-ps-${pseudo_label_model}-pct-gold${percent_gold_label}
-wandb_run_name=ppi_type${ppi_type}-lbda${lbda}-ep${max_epoch}-bs${batch_size}-lr${lr}
+# Set wandb project and run name (use custom values if provided)
+if [ -n "$custom_wandb_project" ]; then
+    wandb_project=$custom_wandb_project
+else
+    wandb_project=ppi-rm-btwn-domain-${formatted_dataset_name}-to-${formatted_target_dataset_name}-pretrain-${formatted_pretrain_model_name}-ps-${pseudo_label_model}-pct-gold${percent_gold_label}
+fi
+
+if [ -n "$custom_wandb_run_name" ]; then
+    wandb_run_name=$custom_wandb_run_name
+else
+    wandb_run_name=ppi_type${ppi_type}-lbda${lbda}-ep${max_epoch}-bs${batch_size}-lr${lr}
+fi
 
 if python check_wandb_run.py --project $wandb_project --name $wandb_run_name; then
     echo "Run is finished"
@@ -58,8 +70,7 @@ else
     # Continue execution to start/resume the run
 fi
 
-# Print the command, then
-
+# Print the command
 echo "deepspeed --module --master_port 12345 openrlhf.cli.train_rm_ppi \
 --save_path ./checkpoint/btwn_domain_rm_ppi \
 --save_pct 1.0 \
